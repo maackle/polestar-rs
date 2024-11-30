4e0f58f5dc68b3de6d2528ce0c47fdd3654bd095
Michael Dougherty <maackle.d@gmail.com>
2024-11-29 20:58:53 -0800

    Two types of consistency checking

--- holochain-model/src/playback.rs
+++ holochain-model/src/playback.rs
@@ -1,5 +1,7 @@
+use std::collections::HashSet;
+
 use itertools::Itertools;
-use polestar::Machine;
+use polestar::{id::Id, Machine};
 
 use crate::{
     op_family::{OpFamilyPhase, OpFamilyState},
@@ -7,46 +9,94 @@ use crate::{
     op_single::{OpPhase, Outcome},
 };
 
-#[test]
-fn test_playback() {
-    type N = u32;
-    type O = u32;
-    type T = u8;
-    // let path = "/home/michael/Holo/chain/crates/holochain/op_events.json";
-    let path = "/tmp/op-events.json";
-    // let path = "/home/michael/proj/polestar-rs/op-events.json";
-    let text = std::fs::read_to_string(path).unwrap();
-    let text = text.lines().join(",");
-    let json = format!("[{}]", text);
+#[cfg(test)]
 
-    let actions: Vec<OpNetworkMachineAction<N, O, T>> = serde_json::from_str(&json).unwrap();
+mod tests {
+    use super::*;
 
-    let machine = OpNetworkMachine::<N, O, T>::new();
-    dbg!(&machine);
-
-    let initial = machine.initial();
-
-    match machine.apply_each_action(initial, actions, |a, s| {
-        eprintln!("action: {:?}", a);
-    }) {
-        Err((e, s, a)) => {
-            eprintln!("state: {:#?}, action: {:#?}", s, a);
-            panic!("{e:?}");
-        }
-        Ok((state, _)) => {
-            let mut expected = OpNetworkState::default();
-            for (n, ops) in state.nodes.iter() {
-                expected.nodes.insert(n.clone(), OpFamilyState::default());
-                for (op, _) in ops.iter() {
-                    expected.nodes.get_mut(n).unwrap().insert(
-                        op.clone(),
-                        OpFamilyPhase::Op(OpPhase::Integrated(Outcome::Accepted)),
-                    );
-                }
+    fn assert_received_integrated<N, O, T>(state: &OpNetworkState<N, O, T>)
+    where
+        N: Id,
+        O: Id,
+        T: Id,
+    {
+        let mut expected = OpNetworkState::default();
+        for (n, ops) in state.nodes.iter() {
+            expected.nodes.insert(n.clone(), OpFamilyState::default());
+            for (op, _) in ops.iter() {
+                expected.nodes.get_mut(n).unwrap().insert(
+                    op.clone(),
+                    OpFamilyPhase::Op(OpPhase::Integrated(Outcome::Accepted)),
+                );
             }
+        }
 
-            pretty_assertions::assert_eq!(state, expected, "all ops must be integrated");
-            dbg!(&state);
+        pretty_assertions::assert_eq!(*state, expected, "all received ops must be integrated");
+    }
+
+    fn assert_all_integrated<N, O, T>(state: &OpNetworkState<N, O, T>)
+    where
+        N: Id,
+        O: Id,
+        T: Id,
+    {
+        let mut nodes = HashSet::new();
+        let mut ops = HashSet::new();
+
+        for (n, op_states) in state.nodes.iter() {
+            nodes.insert(n.clone());
+            ops.extend(op_states.keys().cloned());
+        }
+
+        let mut expected = OpNetworkState::default();
+        for n in nodes {
+            expected.nodes.insert(n.clone(), OpFamilyState::default());
+            for op in ops.iter() {
+                expected.nodes.get_mut(&n).unwrap().insert(
+                    op.clone(),
+                    OpFamilyPhase::Op(OpPhase::Integrated(Outcome::Accepted)),
+                );
+            }
+        }
+
+        pretty_assertions::assert_eq!(*state, expected, "all ops must be integrated");
+    }
+
+    #[test]
+    fn test_playback() {
+        type N = u32;
+        type O = u32;
+        type T = u8;
+        let path = "/tmp/op-events.json";
+        // let path = "/home/michael/Holo/chain/crates/holochain/op_events.json";
+        // let path = "/home/michael/Downloads/op-events.json";
+        // let path = "/home/michael/proj/polestar-rs/op-events.json";
+        let text = std::fs::read_to_string(path).unwrap();
+        let text = text
+            .lines()
+            .filter(|l| !l.is_empty() && !l.starts_with("//"))
+            .join(",");
+        let json = format!("[{}]", text);
+
+        let actions: Vec<OpNetworkMachineAction<N, O, T>> = serde_json::from_str(&json).unwrap();
+
+        let machine = OpNetworkMachine::<N, O, T>::new();
+        dbg!(&machine);
+
+        let initial = machine.initial();
+
+        match machine.apply_each_action(initial, actions, |a, _s| {
+            eprintln!("action: {:?}", a);
+        }) {
+            Err((e, s, a)) => {
+                eprintln!("state: {:#?}, action: {:#?}", s, a);
+                panic!("{e:?}");
+            }
+            Ok((state, _)) => {
+                assert_received_integrated(&state);
+                // assert_all_integrated(&state);
+                dbg!(&state);
+            }
         }
     }
 }
